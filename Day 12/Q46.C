942179a5a62060d57344f3ff908fe9476acd89e1
shloktiwari18 <shlok.tiwari2025@glbajajgroup.org>
2026-06-12 21:15:23 +0530

    Add files via upload

---
+++ Day 12/Q46.C
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int armstrong(int n)
+{
+    int sum = 0, rem, temp;
+
+    temp = n;
+
+    while (n > 0)
+    {
+        rem = n % 10;
+        sum = sum + (rem * rem * rem);
+        n = n / 10;
+    }
+
+    if (sum == temp)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (armstrong(n))
+        printf("Armstrong Number");
+    else
+        printf("Not Armstrong Number");
+
+    return 0;
+}
